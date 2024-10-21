76e94594c3760b9685e2703f8349ac3de55cbe20
Giovanny Otarola <giovannyxdd@gmail.com>
2024-10-21 12:29:38 -0300

    Initial commit

---
+++ src/main/java/formativa/example/formativafs3/Formativafs3Application.java
@@ -0,0 +1,13 @@
+package formativa.example.formativafs3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Formativafs3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Formativafs3Application.class, args);
+	}
+
+}
---
+++ src/test/java/formativa/example/formativafs3/Formativafs3ApplicationTests.java
@@ -0,0 +1,13 @@
+package formativa.example.formativafs3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Formativafs3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
