7015b68827f9aa598ecb8a61793ea85fa2d6664e
Giovanny Otarola <giovannyxdd@gmail.com>
2024-11-04 15:58:19 -0300

    Update LibroController.java

--- src/main/java/com/example/formativafs3/controller/LibroController.java
+++ src/main/java/com/example/formativafs3/controller/LibroController.java
@@ -13,6 +13,7 @@ import com.example.formativafs3.service.*;
 
 @RestController
 @RequestMapping("/api/libros")
+@CrossOrigin(origins = "http://localhost:4200")
 public class LibroController {
 
     @Autowired
